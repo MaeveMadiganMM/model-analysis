dc4b86eec80bd72fd3cc598256433c275868d568
Maeve Madigan <wd296@uni-heidelberg.de>
2024-02-13 20:37:29 +0100

    Add ATLAS analysis and update paths

--- src/data.py
+++ src/data.py
@@ -9,6 +9,8 @@ def preprocess(exp):
 
     if exp.cfg.data.data_label == "CMS_mtt_1D_RunII":
         bins, data, covmat = read_CMS_mtt_1D_RunII(exp)
+    if exp.cfg.data.data_label == "ATLAS_pt_1D_RunII":
+        bins, data, covmat = read_ATLAS_pt_1D_RunII(exp)
     else:
         print("data label not recognised")
         print("data_label", exp.cfg.data.data_label)
@@ -57,4 +59,22 @@ def read_CMS_mtt_1D_RunII(exp):
         np.array(bins, dtype=float),
         np.array(cms_data, dtype=float),
         np.array(cms_covmat, dtype=float),
+    )
+
+
+def read_ATLAS_pt_1D_RunII(exp):
+    """
+    Read and parse the datasets .... ref
+    """
+    atlas_hepdata = np.genfromtxt(exp.cfg.data.data_path, dtype=float,usecols=(0,1,2,))
+    atlas_bins = atlas_hepdata[:,0]
+    atlas_bins = np.append(atlas_bins,atlas_hepdata[-1,1])
+    atlas_data = atlas_hepdata[:,2]
+
+    atlas_covmat = np.genfromtxt(exp.cfg.data.covmat_path, dtype=float)
+
+    return (
+        np.array(atlas_bins, dtype=float),
+        np.array(atlas_data, dtype=float),
+        np.array(atlas_covmat, dtype=float),
     )
--- src/theory.py
+++ src/theory.py
@@ -20,6 +20,9 @@ def signal(exp):
     if exp.modelname == "CMS_mtt_1D_RunII_ALPS":
         signal = signal_CMS_mtt_1D_RunII_ALPS(exp)
         return signal
+    elif exp.modelname == "ATLAS_pt_1D_RunII_ALPS":
+        signal = signal_ATLAS_pt_1D_RunII_ALPS(exp)
+        return signal
     else:
         print("raise error here")  # to do
 
@@ -73,6 +76,54 @@ def signal_CMS_mtt_1D_RunII_ALPS(exp):
     return axion_signal
 
 
+def signal_ATLAS_pt_1D_RunII_ALPS(exp):
+    """
+    Signal model for the analysis of top ALPS using CMS ttbar data.
+
+    Returns
+    -------
+    numpy.ndarray
+        2D array, specifying the SM, quadratic ALP and quartic ALP contributions to
+        the total theory predictions to this dataset, assuming the Wilson coefficient
+        ct=1.
+
+    """
+
+    kfac = kfactor(exp)
+
+    bins_width = exp.bins[1:] - exp.bins[:-1]
+
+    sm = np.divide(
+        float(exp.cfg.model.BR)
+        * np.loadtxt(
+            exp.cfg.model.model_path + "/" + exp.cfg.model.SM_file,
+            dtype=float,
+            usecols=(2,),
+        ),
+        bins_width,
+    )
+    axion_lin = np.divide(
+        float(exp.cfg.model.BR)
+        * np.loadtxt(
+            exp.cfg.model.model_path + "/" + exp.cfg.model.model_file_lin,
+            usecols=(2,),
+            dtype=float,
+        ),
+        bins_width,
+    )
+    axion_quad = np.divide(
+        float(exp.cfg.model.BR)
+        * np.loadtxt(
+            exp.cfg.model.model_path + "/" + exp.cfg.model.model_file_quad,
+            usecols=(2,),
+            dtype=float,
+        ),
+        bins_width,
+    )
+
+    axion_signal = np.c_[kfac * sm, kfac * axion_lin, kfac * axion_quad]
+    return axion_signal
+
 def kfactor(exp):
     """
     QCD kfactor
@@ -85,7 +136,7 @@ def kfactor(exp):
         to the predictions at NLO in QCD
 
     """
-    if exp.cfg.model.kfactor == None:
+    if exp.cfg.model.kfactor is None:
         kfac = np.ones(len(exp.proc_data))
     else:
         kfac = np.loadtxt(
